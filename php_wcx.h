d2438c3a2cb7369f2f3cc1b78dffbe88d5203249
venkman <ixqbar@gmail.com>
2015-02-22 23:50:52 +0800

    verison 0.2.1

--- php_wcx.h
+++ php_wcx.h
@@ -24,7 +24,7 @@
 extern zend_module_entry wcx_module_entry;
 #define phpext_wcx_ptr &wcx_module_entry
 
-#define PHP_WCX_VERSION "0.2.0"
+#define PHP_WCX_VERSION "0.2.1"
 
 #ifdef PHP_WIN32
 #	define PHP_WCX_API __declspec(dllexport)
